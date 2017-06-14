ab0e533b05c4052e47f9eae84df0372a7610be78
Robert Schriver <schriverr@mendel.cgrb.oregonstate.local>
2017-06-13 19:47:37 -0700

    intermediary version

--- example/02_segmentation_stems/xyz_to_oc.py
+++ example/02_segmentation_stems/xyz_to_oc.py
@@ -1,6 +1,7 @@
 import sys
 import csv
 import numpy as np
+import glob
 
 sys.path.append('../00_create_data/')
 import vis
@@ -8,105 +9,69 @@ import vis
 sys.path.append('../../py/')
 import pyoctnet
 
-filename = 'points/points_2_36_tr1.xyz'
-
-with open(filename , 'rb') as csv_file:
-	reader = csv.reader(csv_file)
-	version = reader.next()
-	projection = reader.next()
-	header = reader.next()
-
-	x_index = header.index('X')
-	y_index = header.index('Y')
-	z_index = header.index('Z')
-
-	r_index = header.index('Red')
-	g_index = header.index('Green')
-	b_index = header.index('Blue')
-
-	intensity_index = header.index('Intensity')
+def xyz_to_oc(input_filename, output_rgb_filename, output_classification_filename):
 	
-	classification_index = header.index('Classification')
+	other_classification = 0.01
+	stem_classification = 1.0
+	
+	with open(input_filename , 'rb') as csv_file:
+		reader = csv.reader(csv_file)
+		version = reader.next()
+		projection = reader.next()
+		header = reader.next()
 
-	min_x = 0.0
-	min_y = 0.0
-	min_z = 0.0
+		x_index = header.index('X')
+		y_index = header.index('Y')
+		z_index = header.index('Z')
 
-	max_x = 0.0
-	max_y = 0.0
-	max_z = 0.0
+		r_index = header.index('Red')
+		g_index = header.index('Green')
+		b_index = header.index('Blue')
 
-	xyzs = []
-	features = []
-	classifications_stem = []
-	classifications_other = []
+		intensity_index = header.index('Intensity')
+	
+		classification_index = header.index('Classification')
 
-	for row in reader:
-		x = float(row[x_index])
-		y = float(row[y_index])
-		z = float(row[z_index])
+		xyzs = []
+		features = []
+		classifications = []
+		classifications_other = []
 
-		intensity = float(row[intensity_index])
+		for row in reader:
+			x = float(row[x_index])
+			y = float(row[y_index])
+			z = float(row[z_index])
+
+			intensity = float(row[intensity_index])
 		
-		r = int(float(row[r_index]) - intensity)
-		g = int(float(row[g_index]) - intensity)
-		b = int(float(row[b_index]) - intensity)
+			r = (float(row[r_index]) - intensity) / 255
+			g = (float(row[g_index]) - intensity) / 255
+			b = (float(row[b_index]) - intensity) / 255
 		
-		classification = int(float(row[classification_index]))
+			classification = int(float(row[classification_index]))
 		
-		xyzs.append((x, y, z))
-		features.append((r, g, b))
+			xyzs.append((x, y, z))
+			features.append((r, g, b))
 
-		if classification == 20:
-			classifications_stem.append((1, 1))
-			classifications_other.append((0, 0))
-		else:
-			classifications_other.append((1, 1))
-			classifications_stem.append((0, 0))
+			if classification == 20:
+				classifications.append((stem_classification, 1.0))
+			else:
+				classifications.append((other_classification, 1.0))
+		vx_res = 64
 
-		if x < min_x:
-			min_x = x
+		oc_from_xyz_rgb = pyoctnet.Octree.create_from_pc(np.asarray(xyzs, dtype=np.float32), np.asarray(features, dtype=np.float32), vx_res, vx_res, vx_res, normalize=True)
 
-		if y < min_y:
-			min_y = y
+		vis.write_ply_voxels(output_rgb_filename, oc_from_xyz_rgb.to_cdhw()[0])
 
-		if z < min_z:
-			min_z = z
+		oc_from_xyz_classification = pyoctnet.Octree.create_from_pc(np.asarray(xyzs, dtype=np.float32), np.asarray(classifications, dtype=np.float32), vx_res, vx_res, vx_res, normalize=True)
+
+		vis.write_ply_voxels(output_classification_filename, oc_from_xyz_classification.to_cdhw()[0])
+
+train_path = 'train/xyz'
+test_path = 'test/xyz'
+
+xyz_ext = '.xyz'
+ply_ext = '.ply'
 
 
-		if x > max_x:
-			max_x = x
 
-		if y > max_y:
-			max_y = y
-		
-		if z > max_z:
-			max_z = z
-
-	print (min_x, min_y, min_z)
-	print (max_x, max_y, max_z)
-
-	shifted_min_x = 0.0
-	shifted_max_x = max_x - min_x
-
-	shifted_min_y = 0.0
-	shifted_max_y = max_y - min_y
-
-	shifted_min_z = 0.0
-	shifted_max_z = max_z - min_z
-
-	print (shifted_max_x, shifted_max_y, shifted_max_z)
-
-	vx_res = 64
-
-	oc_from_xyz_rgb = pyoctnet.Octree.create_from_pc(np.asarray(xyzs, dtype=np.float32), np.asarray(features, dtype=np.float32), vx_res, vx_res, vx_res, normalize=True)
-
-	vis.write_ply_voxels('oc_from_xyz_rgb_64.ply', oc_from_xyz_rgb.to_cdhw()[0])
-
-	oc_from_xyz_stem = pyoctnet.Octree.create_from_pc(np.asarray(xyzs, dtype=np.float32), np.asarray(classifications_stem, dtype=np.float32), vx_res, vx_res, vx_res, normalize=True)
-
-	vis.write_ply_voxels('oc_from_xyz_stem_64.ply', oc_from_xyz_stem.to_cdhw()[0], color=[255, 255, 255])
-
-	oc_from_xyz_other = pyoctnet.Octree.create_from_pc(np.asarray(xyzs, dtype=np.float32), np.asarray(classifications_other, dtype=np.float32), vx_res, vx_res, vx_res, normalize=True)
-
-	vis.write_ply_voxels('oc_from_xyz_other_64.ply', oc_from_xyz_other.to_cdhw()[0], color=[255, 255, 255])
